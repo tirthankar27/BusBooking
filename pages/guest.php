c48893ac322604685b4ffe2d2bd13092c789c7ba
tirthankar27 <tirthankarghosh4@gmail.com>
2024-12-20 20:40:41 +0530

    Updated header and footer

--- pages/guest.php
+++ pages/guest.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BookMyTrip</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <!-- <base href="/BusBooking/"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css">
 </head>
 <body>
     <?php include 'header.php'; ?>
--- pages/header.php
+++ pages/header.php
@@ -6,7 +6,7 @@
             <a href="pages/login.php"class="text-lg font-semibold text-green-700 hover:text-green-900">Login</a>
             <a href="pages/signup.php"class="text-lg font-semibold text-green-700 hover:text-green-900">Signup</a>
         <?php else: ?>
-            <a href="" class="text-lg font-semibold text-green-700 hover:text-green-900">Contact Us</a>
+            <a href="" class="text-lg font-semibold text-green-700 hover:text-green-900"><?php echo $_SESSION['name']; ?></a>
             <a href="pages/bookings.php"class="text-lg font-semibold text-green-700 hover:text-green-900">My Bookings</a>
             <a href="pages/logout.php"class="text-lg font-semibold text-green-700 hover:text-green-900">Logout</a>
         <?php endif; ?>
