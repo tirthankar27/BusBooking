4aff33e4d6f5f0d39a5f2d109f3ae82ca279041c
tirthankar27 <tirthankarghosh4@gmail.com>
2025-01-14 22:42:01 +0530

    Made success page

--- backend/paymentBackend.php
+++ backend/paymentBackend.php
@@ -10,7 +10,7 @@
         $stmt = $conn->prepare("INSERT INTO bookings (passenger, source, dest, doj, seat, fare, email) VALUES (?,?,?,?,?,?,?)");
         $stmt->bind_param('sssssss', $_SESSION['passenger'], $_SESSION['source'], $_SESSION['dest'], $_SESSION['date'], $_SESSION['seat'], $_SESSION['fare'], $_SESSION['email']);
         if($stmt->execute()) {
-            echo "<script>alert('Payment successfull');window.location.href='../main.php';</script>";
+            echo "<script>alert('Payment successfull');window.location.href='successFrontend.php';</script>";
             exit();
         }
         $stmt->close();
